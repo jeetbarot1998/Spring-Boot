f4c18d1d09367c1e3223d1a08ac9ffdd64d724eb
Jeet Barot <jeetbarot1998@gmail.com>
2024-05-13 09:05:17 -0400

    OAuth2 Changes

--- src/main/java/com/demo/springboot/SpringBootApp/SpringBootAppApplication.java
+++ src/main/java/com/demo/springboot/SpringBootApp/SpringBootAppApplication.java
@@ -2,7 +2,9 @@ package com.demo.springboot.SpringBootApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages =
@@ -10,6 +12,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 		"com.demo.springboot.SpringBootApp.courses",
 		"com.demo.springboot.SpringBootApp.securityExample",
 		"com.demo.springboot.SpringBootApp.JWT"})
+@EnableOAuth2Sso
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
